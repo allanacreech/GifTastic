7924e5ec1e742ea1425ba6741250a76bd71a5b11
Allana Creech <allanacreech@gmail.com>
2016-10-26 21:56:38 -0400

    basci website layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+
+//Array to hold different area's for giphy villains
+
+var villainsList = [
+	"Disney Villains",
+	"Marvel Villains",
+	"Batman Villains",
+	"Superman Villains",
+	"Book Villains",
+	"Lego Villains",
+	"Super Saiyan Villains"
+];
+
+$(document).ready(function(){
+	$("button").click(function(){
+		alert("you clicked me");
+	})
+});///why does it alert twice for each click?
+
